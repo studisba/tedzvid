350f1ea6697953abba9fbec845741c29db72ec9f
FG <fikretgogic@gmail.com>
2019-05-08 14:12:27 +0200

    Added fontawesome packages and Underline C

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.scss';
 import {Container, Row, Col} from 'react-bootstrap';
+import Underline from './Letters/Underline';
 
 function App() {
   return (
     <Container>
       <Row>
         <Col>Tedžvid</Col>
+      </Row>
+      <Row>
+        <Col>
+          <Underline>
+            A
+          </Underline>
+        </Col>
       </Row>
     </Container>
   );
---
+++ src/Letters/Underline.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Underline (props) {
+    return <div className="underLine"> 
+        { props.children }
+    </div>
+}
+
+export default Underline;
