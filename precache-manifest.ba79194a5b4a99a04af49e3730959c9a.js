a43ec812c060431322b0563dbe2b9244a55aa964
Lilium <fikretgogic@gmail.com>
2019-05-10 21:07:57 +0200

    Updates

---
+++ precache-manifest.ba79194a5b4a99a04af49e3730959c9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ceaa54279a92f9db1f57c774c3819b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea9374e3c9a432d517af",
+    "url": "/static/css/main.8e51b751.chunk.css"
+  },
+  {
+    "revision": "5140be5d8887c715d117",
+    "url": "/static/js/2.458e8231.chunk.js"
+  },
+  {
+    "revision": "ea9374e3c9a432d517af",
+    "url": "/static/js/main.b8513e52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
