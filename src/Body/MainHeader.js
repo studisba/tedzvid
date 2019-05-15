8942f4daed8333490a76a2a923100d4895be234a
FG <fikretgogic@gmail.com>
2019-05-15 10:01:47 +0200

    Menu updated

--- src/Body/MainHeader.js
+++ src/Body/MainHeader.js
@@ -19,7 +19,7 @@ function Header() {
             </ul>
         </nav>;
     } if (opened) {
-        return <nav onClick={menuClickHandler} className="mainmenu">
+        return <nav onMouseLeave={menuClickHandler} className="mainmenu">
             <ul className="mainmenulist">
                 <li className="mainmenuitem">Lekcije ١</li>
                 <li className="mainmenuitem">Lekcije ٢</li>
@@ -27,38 +27,41 @@ function Header() {
                 <li className="mainmenuitem">Lekcije ٤</li>
                 <li className="mainmenuitem">Lekcije ٥</li>
             </ul>
+
+            <div className="bottomMenuMain">
             <ul className="bottomMenu">
-                <Link to="/lekcija1" className="text-white"><li className="bottommenuitem">Lekcija 1</li></Link>
-                <Link to="/lekcija6" className="text-white"><li className="bottommenuitem">Lekcija 6</li></Link>
-                <li className="bottommenuitem">Lekcija 11</li>
-                <li className="bottommenuitem">Lekcija 16</li>
-                <li className="bottommenuitem">Lekcija 21</li>
+                <Link to="/lekcija1" className="menulink"><li className="bottommenuitem">Lekcija 1</li></Link>
+                <Link to="/lekcija2" className="menulink"><li className="bottommenuitem">Lekcija 2</li></Link>
+                <Link to="/lekcija3" className="menulink"><li className="bottommenuitem">Lekcija 3</li></Link>
+                <Link to="/lekcija4" className="menulink"><li className="bottommenuitem">Lekcija 4</li></Link>
+                <Link to="/lekcija5" className="menulink"><li className="bottommenuitem">Lekcija 5</li></Link>
             </ul>
             <ul className="bottomMenu">
-                <Link to="/lekcija2" className="text-white"><li className="bottommenuitem">Lekcija 2</li></Link>
-                <Link to="/lekcija7" className="text-white"><li className="bottommenuitem">Lekcija 7</li></Link>
-                <li className="bottommenuitem">Lekcija 12</li>
-                <li className="bottommenuitem">Lekcija 17</li>
-                <li className="bottommenuitem">Lekcija 22</li>
+                <Link to="/lekcija6" className="menulink"><li className="bottommenuitem">Lekcija 6</li></Link>
+                <Link to="/lekcija7" className="menulink"><li className="bottommenuitem">Lekcija 7</li></Link>
+                <Link to="/lekcija8" className="menulink"><li className="bottommenuitem">Lekcija 8</li></Link>
+                <Link to="/lekcija9" className="menulink"><li className="bottommenuitem">Lekcija 9</li></Link>
+                <Link to="/lekcija10" className="menulink"><li className="bottommenuitem">Lekcija 10</li></Link>
             </ul>
             <ul className="bottomMenu">
-                <Link to="/lekcija3" className="text-white"><li className="bottommenuitem">Lekcija 3</li></Link>
-                <Link to="/lekcija8" className="text-white"><li className="bottommenuitem">Lekcija 8</li></Link>
-                <li className="bottommenuitem">Lekcija 13</li>
-                <li className="bottommenuitem">Lekcija 18</li>
+                <Link to="/lekcija11" className="menulink"><li className="bottommenuitem">Lekcija 11</li></Link>
+                <Link to="/lekcija12" className="menulink"><li className="bottommenuitem">Lekcija 12</li></Link>
+                <Link to="/lekcija13" className="menulink"><li className="bottommenuitem">Lekcija 13</li></Link>
+                <Link to="/lekcija14" className="menulink"><li className="bottommenuitem">Lekcija 14</li></Link>
+                <Link to="/lekcija15" className="menulink"><li className="bottommenuitem">Lekcija 15</li></Link>
             </ul>
             <ul className="bottomMenu">
-                <Link to="/lekcija4" className="text-white"><li className="bottommenuitem">Lekcija 4</li></Link>
-                <Link to="/lekcija9" className="text-white"><li className="bottommenuitem">Lekcija 9</li></Link>
-                <li className="bottommenuitem">Lekcija 14</li>
-                <li className="bottommenuitem">Lekcija 19</li>
+                <Link to="/lekcija16" className="menulink"><li className="bottommenuitem">Lekcija 16</li></Link>
+                <Link to="/lekcija17" className="menulink"><li className="bottommenuitem">Lekcija 17</li></Link>
+                <Link to="/lekcija18" className="menulink"><li className="bottommenuitem">Lekcija 18</li></Link>
+                <Link to="/lekcija19" className="menulink"><li className="bottommenuitem">Lekcija 19</li></Link>
+                <Link to="/lekcija20" className="menulink"><li className="bottommenuitem">Lekcija 20</li></Link>
             </ul>
             <ul className="bottomMenu">
-                <Link to="/lekcija5" className="text-white"><li className="bottommenuitem">Lekcija 5</li></Link>
-                <Link to="/lekcija10" className="text-white"><li className="bottommenuitem">Lekcija 10</li></Link>
-                <li className="bottommenuitem">Lekcija 15</li>
-                <li className="bottommenuitem">Lekcija 20</li>
+                <Link to="/lekcija21" className="menulink"><li className="bottommenuitem">Lekcija 21</li></Link>
+                <Link to="/lekcija22" className="menulink"><li className="bottommenuitem">Lekcija 22</li></Link>
             </ul>
+            </div>
         </nav>;
     }
 }
