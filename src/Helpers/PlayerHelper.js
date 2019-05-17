931396a845819e073bc0ac15ae19898b3f54c30d
FG <fikretgogic@gmail.com>
2019-05-17 15:16:08 +0200

    Added player audio helper

--- src/Helpers/PlayerHelper.js
+++ src/Helpers/PlayerHelper.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Player from '../Player/Player';
 import Arabic from '../Letters/Arabic';
 
 function PRow(data, rowname) {
+    const [playing, setPlaying] = useState(false);
+    const toggle = () => setPlaying(!playing);
+    
     const PlayerRow = (datarr, rowname) => {
         const row = datarr[rowname].map((dat) => {
-        return <span key={'key' + dat.id}>
-        <Player url={dat.url} key={'p' + dat.id}>
-            <Arabic
-            arabic={dat.highlight}
-            key={'a' + dat.id}
-            >{dat.word}</Arabic>
-        </Player> {dat.after === 'break' ? <br/> : dat.after}
+        return <span key={'key' + dat.id} onClick={toggle}>
+            <Player url={dat.url} key={'p' + dat.id} playr={playing ? true : false}>
+                <Arabic
+                arabic={dat.highlight}
+                key={'a' + dat.id}
+                >{dat.word}</Arabic>
+            </Player> {dat.after === 'break' ? <br/> : dat.after}
         </span>
         });
     
