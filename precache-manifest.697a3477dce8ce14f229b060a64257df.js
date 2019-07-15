52bc677b00c386a600c6d83c550fc64cf3655c47
FG <fikretgogic@gmail.com>
2019-07-15 14:57:14 +0200

    Updates

---
+++ precache-manifest.697a3477dce8ce14f229b060a64257df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0465550b7bbdeefdbe5d5f8adf3ad3d0",
+    "url": "/tedzvid/index.html"
+  },
+  {
+    "revision": "f0a12836d37bc0743567",
+    "url": "/tedzvid/static/css/main.75194660.chunk.css"
+  },
+  {
+    "revision": "ca42be40e442dec7539c",
+    "url": "/tedzvid/static/js/2.b0f7c6d2.chunk.js"
+  },
+  {
+    "revision": "f0a12836d37bc0743567",
+    "url": "/tedzvid/static/js/main.b07ba80b.chunk.js"
+  },
+  {
+    "revision": "6038425b23784e662bdc",
+    "url": "/tedzvid/static/js/runtime~main.c894e1fc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ba79194a5b4a99a04af49e3730959c9a.js"
+  "/tedzvid/precache-manifest.697a3477dce8ce14f229b060a64257df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tedzvid/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
